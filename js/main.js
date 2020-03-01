b014c2da263048393f3d483386c205bee71559da
Kamran Mackey <kamranm1200@gmail.com>
2020-02-29 23:16:35 -0700

    Added OS-dependent dark theme support.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
+/**
+ * Copyright (c) Kamran Mackey, 2020-present. All Rights Reserved.
+ */
+
 console.log("You have reached the dragon's den (the Developer Tools console). Please tread carefully.");
