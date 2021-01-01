d8054d289152fa580622dff0d43fa3de014680c7
Kamran Mackey <kamranm1200@gmail.com>
2020-12-31 21:37:17 -0700

    core: Revamped core website; now using Bootstrap 4.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- js/main.js
+++
@@ -1,5 +0,0 @@
-/**
- * Copyright (c) Kamran Mackey, 2020-present. All Rights Reserved.
- */
-
-console.log("You have reached the dragon's den (the Developer Tools console). Please tread carefully.");
