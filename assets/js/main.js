2f0527c94954c11e10e1d2ea789643693a3d3cff
Kamran Mackey <kamranm1200@gmail.com>
2021-01-26 11:28:13 -0700

    Implement displaying copyright year in footer.
    The text is centered now as well.
    
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 console.log("You have entered the dragon's den (the Developer Tools). Please tread carefully.");
+
+let year_element = document.getElementById('year');
+let year = new Date().getFullYear();
+
+function write_year(text) {
+    return document.createTextNode(text);
+}
+
+year_element.appendChild(write_year(year));
